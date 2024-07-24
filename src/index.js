de53ea16a15dac15e25b909ec235c3b04f1fcd9d
Miss-Shubhangi <shubhangi.s.pawar2007@gmail.com>
2024-07-24 18:56:29 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDom from "react-dom/client"
+
+
+const root =ReactDom.createRoot(document.getElementById("root"))
+
+root.render(<h1>Hello Everyone ....I'm Shubhangi Santosh Pawar. </h1> )
